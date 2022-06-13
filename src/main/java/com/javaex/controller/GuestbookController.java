6a41cb03d000afa256909236bf9547abbedda14d
GyeongAe Kim <kgae128@gmail.com>
2022-06-13 17:37:51 +0900

    guestbookController

--- src/main/java/com/javaex/controller/GuestbookController.java
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -5,10 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.dao.GuestbookDao;
 import com.javaex.vo.GuestbookVo;
@@ -19,11 +17,10 @@ public class GuestbookController {
 	//생성자
 	//메소드gs
 	//메소드일반
-	//리스트
+	//1. 리스트
 	@RequestMapping(value="/list", method={RequestMethod.GET, RequestMethod.POST})
 	public String list(Model model) {
 		System.out.println("GuestbookController>list()");
-		
 		
 		//Dao - getList꺼내오기
 		GuestbookDao guestbookDao = new GuestbookDao();
@@ -36,13 +33,14 @@ public class GuestbookController {
 		return "/WEB-INF/views/list.jsp";
 	}
 	
-	//게스트북 등록
+	
+	//2. 방명록 등록
 	@RequestMapping(value="/write", method={RequestMethod.GET, RequestMethod.POST})
 	public String write(@ModelAttribute GuestbookVo guestbookVo) {
 		System.out.println("GuestbookController>write()");
 		
-		//파라미터 꺼낼 필요 없어짐
-		//파라미터들을 한꺼번에 personVo로 꺼냈음-> 개별의 파라미터들을 vo로 묶을 필요가 없음
+		//파라미터들을 한꺼번에 personVo로 꺼냈음 
+		//->파라미터 꺼낼 필요 없음, 개별의 파라미터들을 vo로 묶을 필요가 없음
 		
 		//dao로 저장하기
 		GuestbookDao guestbookDao = new GuestbookDao();
@@ -51,7 +49,8 @@ public class GuestbookController {
 		return "redirect:/list";
 	}
 	
-	//게스트북 삭제폼
+	
+	//3. 게스트북 삭제폼
 	@RequestMapping(value="/deleteForm", method={RequestMethod.GET, RequestMethod.POST})
 	public String deleteForm() {
 		System.out.println("GuestbookController>deleteForm()");
@@ -59,7 +58,8 @@ public class GuestbookController {
 		return "/WEB-INF/views/deleteForm.jsp";
 	}
 	
-	//게스트북 삭제
+	
+	//4. 게스트북 삭제
 	@RequestMapping(value="/delete", method={RequestMethod.GET, RequestMethod.POST})
 	public String delete(@ModelAttribute GuestbookVo guestbookVo) {
 		System.out.println("GuestbookController>delete()");
